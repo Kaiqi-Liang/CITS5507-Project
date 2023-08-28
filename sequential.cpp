4aa0853ef55452537611aaca5db9b2324460c9b4
Kaiqi Liang <kaiqi.liang@outlook.com>
2023-08-28 18:21:45 +0800

    Add setup function

--- sequential.cpp
+++ sequential.cpp
@@ -3,10 +3,19 @@
 #include <iostream>
 
 #define NUM_OF_STEPS 100
+#define NUM_OF_STEPS 100
+#define SQUARE 100
+
+void setup() {
+	// Generate initial location for all the fish randomly
+}
 
 int main() {
 	clock_t begin = std::clock();
 	clock_t end = std::clock();
+
+	setup();
+	
 	double time_spent = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
 	std::cout << "time spent:" << std::fixed << std::setw(10)
 	          << std::setprecision(6) << time_spent << std::endl;
