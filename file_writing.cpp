7a9ba7f080cece113c7f5c29dcd75c1ee2bd6a8b
Kaiqi Liang <kaiqi.liang@outlook.com>
2023-10-07 15:57:34 +0800

    Add const to variable declarations

--- file_writing.cpp
+++ file_writing.cpp
@@ -25,7 +25,7 @@ MPI_Datatype create_mpi_struct() {
 	std::fill_n(types, 4, MPI_DOUBLE);
 	int blocklengths[NUM_FIELDS];
 	std::fill_n(blocklengths, 4, 1);
-	MPI_Aint offsets[NUM_FIELDS] = {
+	const MPI_Aint offsets[NUM_FIELDS] = {
 	    offsetof(Fish, distance_),
 	    offsetof(Fish, weight_),
 	    offsetof(Fish, x_),
@@ -40,14 +40,14 @@ int main() {
 	std::vector<Fish> send_buf;
 	int process_id, num_processes;
 	MPI_Init(0, 0);
-	MPI_Datatype MPI_FISH = create_mpi_struct();
+	const MPI_Datatype MPI_FISH = create_mpi_struct();
 	MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
 	if (process_id == MASTER) {
 		send_buf = std::vector<Fish>(NUM_FISH);
 		write_fish(send_buf, std::string{"before"});
 	}
-	int num_fish_per_process = NUM_FISH / num_processes;
+	const int num_fish_per_process = NUM_FISH / num_processes;
 	std::vector<Fish> recv_buf(num_fish_per_process);
 	MPI_Scatter(
 	    send_buf.data(),
