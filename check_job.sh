cd193f9ec62f389d993d1a1acbf362e9a023fc37
Kaiqi Liang <kaiqi.liang@outlook.com>
2023-10-12 22:28:48 +0800

    Add script to check job

---
+++ check_job.sh
@@ -0,0 +1,14 @@
+jobs=5
+while true
+do
+	curr=`squeue | grep kliang1 | wc -l`
+	if [[ $curr -eq 0 ]]
+	then
+		exit
+	elif [[ $curr -lt $jobs ]]
+	then
+		squeue | grep kliang1 | head -1
+		jobs=$curr
+	fi
+	sleep 30
+done
